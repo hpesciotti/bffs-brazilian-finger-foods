4d87884838d79c29d0113221aff3bb0f924bbbd8
Henrique Pesciotti <hpesciotti@gmail.com>
2024-12-30 21:47:14 +0000

    feat: add product_detail pages

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Batch
 
 # Create your views here.
@@ -15,10 +15,10 @@ def all_products(request):
     # Gets the offer batch for sale first (qty > 0 and offer = 2)
     for product in products:
         product.discount_price_batch = product.batches.filter(quantity__gt=0, offer=2).first()
-        
+
         # original price
         product.original_price_batch = product.batches.filter(quantity__gt=0).first()
-        
+
         product.dietary_categories_names = product.dietary_categories.values_list('name', flat=True)
 
     context = {
@@ -26,4 +26,26 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
-    
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product.objects.prefetch_related(
+        'batches', 'dietary_categories'
+    ), pk=product_id)
+
+    # Gets the offer batch for sale first (qty > 0 and offer = 2)
+    discount_price_batch = product.batches.filter(quantity__gt=0, offer=2).first()
+    original_price_batch = product.batches.filter(quantity__gt=0).first()
+
+    # Gets dietaries categories for each products
+    dietary_categories_names = product.dietary_categories.values_list('name', flat=True)
+
+    context = {
+        'product': product,
+        'discount_price_batch': discount_price_batch,
+        'original_price_batch': original_price_batch,
+        'dietary_categories_names': dietary_categories_names,
+    }
+
+    return render(request, 'products/product_detail.html', context)
