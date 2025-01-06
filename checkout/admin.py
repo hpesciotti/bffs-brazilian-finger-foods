c0c86d7391255d28a175e558301798dfb72bac80
Henrique Pesciotti <hpesciotti@gmail.com>
2025-01-06 21:03:18 +0000

    feat/bug: bug connect with the incopatibility between bootstrap 5 and crispyforms

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
 # Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    Add order line to admin panel
+    """
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Add order to admin panel
+    """
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                        'delivery_cost', 'order_total',
+                        'grand_total', 'original_bag')
+
+    fields = ('order_number', 'date', 'full_name', 'email',
+             'phone_number', 'eircode', 'street_address1',
+             'street_address2', 'delivery_cost', 'order_total',
+             'grand_total','original_bag')
+
+    list_display = ('order_number', 'date', 'full_name',
+                     'order_total', 'delivery_cost', 'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -3,8 +3,8 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-from products.models import Batch
 from django.core.validators import RegexValidator
+from products.models import Batch
 
 # Create your models here.
 
