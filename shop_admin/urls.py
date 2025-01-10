eccbb3a303ca8300ed4b61e6f29ffdbde7ef9321
Henrique Pesciotti <hpesciotti@gmail.com>
2025-01-10 21:51:07 +0000

    feat: add manage_batch

--- shop_admin/urls.py
+++ shop_admin/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.admin_panel, name='shop_admin'),
     path('add_batch/', views.add_batch, name='add_batch'),
+    path('manage_batches/', views.manage_batches, name='manage_batches')
 ]
--- shop_admin/views.py
+++ shop_admin/views.py
@@ -13,6 +13,15 @@ from .forms import BatchForm
 
 @login_required
 def admin_panel(request):
+    """ Displays all the shop managing options in a menu """
+    
+    if not request.user.is_superuser:
+        return render(request, '403.html')
+    
+    return render(request, 'shop_admin/admin_panel.html')
+
+@login_required
+def manage_batches(request):
     """ View to display all batches ordered by batch.id with sorting and search """
     
     # Sorting Fields
@@ -31,11 +40,17 @@ def admin_panel(request):
     batches = Batch.objects.all()
 
     if search_query:
+        print(f"Search query: {search_query}")
+        print(f"Batches before filter: {batches.count()}")
         batches = batches.filter(
             Q(batch_number__icontains=search_query) |
-            Q(product__name__icontains=search_query) |
-            Q(expiry_date__icontains=search_query)
+            Q(product__short_widget_name__icontains=search_query) |
+            Q(expiry_date__icontains=search_query) |
+            Q(sale_price__icontains=search_query) |
+            Q(offer__icontains=search_query) |
+            Q(quantity__icontains=search_query)
         )
+        print(f"Batches after filter: {batches.count()}")
 
     if sort_order == 'desc':
         batches = batches.order_by(f'-{sort_by}')
@@ -48,7 +63,7 @@ def admin_panel(request):
         'sort_order': sort_order,
         'search_query': search_query,
     }
-    return render(request, 'shop_admin/admin_panel.html', context)
+    return render(request, 'shop_admin/manage_batches.html', context)
 
 @login_required
 def add_batch(request):
@@ -62,7 +77,7 @@ def add_batch(request):
         if form.is_valid():
             batch = form.save()
             messages.success(request, 'Successfully added batch!')
-            return redirect(reverse('batch_detail', args=[batch.id]))  # You may need to define a batch detail view
+            return redirect(reverse('batch_detail', args=[batch.id]))
         else:
             messages.error(request, 'Failed to add batch. Please ensure the form is valid.')
     else:
