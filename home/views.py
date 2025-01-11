1e2f2f78ffda51af766655aab0794212469f34f6
Henrique Pesciotti <hpesciotti@gmail.com>
2025-01-11 23:02:45 +0000

    chore: validate python linter home

--- home/views.py
+++ home/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from products.models import Product, Batch
 
-# Create your views here.
 
 def index(request):
-    """ 
-    A view to return the index page 
+    """
+    A view to return the index page
     """
 
-    best_seller_products =  Product.objects.prefetch_related(
+    best_seller_products = Product.objects.prefetch_related(
         'batches', 'dietary_categories'
     ).filter(best_seller=True)
 
@@ -18,21 +17,29 @@ def index(request):
 
     # Gets the offer batch for sale first (qty > 0 and offer = 2)
     for product in offer_products:
-        product.discount_price_batch = product.batches.filter(quantity__gt=0, offer=2).first()
+        product.discount_price_batch = product.batches.filter(
+            quantity__gt=0, offer=2).first()
 
         # original price
-        product.original_price_batch = product.batches.filter(quantity__gt=0).first()
+        product.original_price_batch = product.batches.filter(
+            quantity__gt=0).first()
 
-        product.dietary_categories_names = product.dietary_categories.values_list('name', flat=True)
+        product.dietary_categories_names = (
+            product.dietary_categories.values_list(
+                'name', flat=True))
 
     for product in best_seller_products:
-        product.discount_price_batch = product.batches.filter(quantity__gt=0, offer=2).first()
+        product.discount_price_batch = product.batches.filter(
+            quantity__gt=0, offer=2).first()
 
         # original price
-        product.original_price_batch = product.batches.filter(quantity__gt=0).first()
+        product.original_price_batch = (
+            product.batches.filter(
+                quantity__gt=0).first())
 
-        product.dietary_categories_names = product.dietary_categories.values_list('name', flat=True)
-
+        product.dietary_categories_names = (
+            product.dietary_categories.values_list(
+                'name', flat=True))
 
     context = {
         'best_seller_products': best_seller_products,
@@ -41,14 +48,18 @@ def index(request):
 
     return render(request, 'home/index.html', context)
 
+
 def custom_403_view(request, exception):
     return render(request, '403.html', status=403)
+
 
 def custom_404_view(request, exception):
     return render(request, '404.html', status=404)
 
+
 def custom_500_view(request, exception=None):
     return render(request, '500.html', status=500)
+
 
 def contacts(request):
     """Contatcs view with static information"""
